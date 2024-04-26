02960b235345da652712d8617680381a25e76c36
Muhammad Abdullah Zahid <119707334+abdullah1904@users.noreply.github.com>
2024-04-26 10:36:17 +0500

    User Creation Automation

--- Automation.sh
+++ Automation.sh
@@ -2,32 +2,41 @@
 
 while [ 1 -eq 1 ]
 do
-    clear
-    echo -e "Automations: \n"
-    echo "1) User Automation"
-    echo "2) File Automation"
-    echo -e "3) Exit\n"
-    read -p "Select Option: " option
-    if [ $option -eq 1 ]
-    then
-        clear
-        echo "User Automation"
-        read -p "Press any key to continue: "
-    elif [ $option -eq 2 ]
-    then
-        clear
-        echo "File Automation"
-        read -p "Press any key to continue: "
-    elif [ $option -eq 3 ]
-    then
-        clear
-        echo -e "\nThank You ;)\n"
-        read -p "Press any key to continue: "
-        clear
-        break
-    else
-        clear
-        echo "Invalid Option"
-        read -p "Press any key to continue: "
-    fi
+   clear
+   echo -e "Automations: \n"
+   echo "1) User Automation"
+   echo "2) File Automation"
+   echo -e "3) Exit\n"
+   read -p "Select Option: " option
+   if [ $option -eq 1 ]
+   then
+	clear
+	echo "User Automation"
+	read -p "Enter Username: " username
+	read -sp "Enter Password: " password
+	sudo useradd -m -s /bin/bash $username
+	echo "$username:$password" | sudo chpasswd
+	read -p "Do you want to change user(Y/N): " option
+	if [ $option = 'Y' ]
+	then
+		su username
+	fi
+	read -p "Press any key to continue: "
+   elif [ $option -eq 2 ]
+   then
+	clear
+	echo "File Automation"
+	read -p "Press any key to continue: "
+   elif [ $option -eq 3 ]
+   then
+	clear
+	echo -e "\nThank You ;)\n"
+	read -p "Press any key to continue: "
+	clear
+	break
+   else
+	clear
+	echo "Invalid Option"
+	read -p "Press any key to continue: "
+   fi
 done
